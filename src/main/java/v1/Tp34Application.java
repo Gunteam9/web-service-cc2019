d9059c462043f7d91cceb417b2fae655e1f42d65
Gunteam9 <58592342+Gunteam9@users.noreply.github.com>
2021-04-14 15:52:21 +0200

    Initial commit

---
+++ src/main/java/v1/Tp34Application.java
@@ -0,0 +1,13 @@
+package v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp34Application.class, args);
+	}
+
+}
